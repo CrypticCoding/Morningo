bbe0b0ae33375a2a84ff935e4f7067009096b510
CrypticCoding <crypticcoding245@gmail.com>
2021-10-31 11:45:28 +0600

    baseline build

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,36 @@
+import 'package:flutter/services.dart';
+import 'package:morningo/Models/NavigatorController.dart';
 import 'package:flutter/material.dart';
-import 'package:morningo/Pages/SetupPage.dart';
 import 'package:morningo/Pages/Splash_Screen.dart';
 
-// void main() {
-//   runApp(HomeSplashScreen());
-// }
+/*
+   TODO:
+   1) One Time, On Null showing on Activities other times its not fix it to show only once (DONE)
+   2) Bug Fix: SharedPreference on TodoCard How Many Left, Activities Time 
+   3) Pop UP Box, When Morning Star Adds
+   4) Fucking BUG ABOUT THE DICK BUTTONS NOT WORKING! (DONE) (FIXED)
+
+    //var date = DateTime.now(); 
+  var newDate = DateTime.yesterday(); // get this from the SharedPrefrence,  
+  final difference = date / newDate.difference(date / newDate).inHours;
+  print("Time Differece: ${difference * -1} Hours");
+    
+  if (difference == 0) {
+    print("Time To Change!"); // Unlock Another Wake Up Time
+  }
+
+*/
+
 void main() {
-  runApp(SetupPage());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(
+    MaterialApp(
+      theme: ThemeData(fontFamily: 'CaviarDreams'),
+      home: AppSplashScreen(),
+    ),
+  );
 }
