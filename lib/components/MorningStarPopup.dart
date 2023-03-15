1f9e4ff06838f3128fe4731bc977d992a39e1136
Saugat Siddiky Jarif <43694237+CrypticCoding@users.noreply.github.com>
2023-03-15 11:56:47 +0600

    Update MorningStarPopup.dart

--- lib/components/MorningStarPopup.dart
+++ lib/components/MorningStarPopup.dart
@@ -1,4 +1,4 @@
-import 'package:adobe_xd/pinned.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_svg/flutter_svg.dart';
